85d53a02e2c40cca4d73af97e7566ba39fb94397
AkashiAkatsuki <katsuki@sakplus.jp>
2017-04-21 22:59:42 +0900

    Gitマジやばくね

--- db/seeds.rb
+++
@@ -1,6 +0,0 @@
-Client.create(
-  consumer_key:        "mA8LJOHxXRqpR4CnUM1feXnSB",
-  consumer_secret:     "Lj2W0yBJJxgTMa7SPDnEWFzelwPRdEVwPwsY30k8ZaPCcs5rns",
-  access_token:        "833207117288910848-z4gA7DU6IkeG4P3vJyTn2JCvuWnTUuj",
-  access_token_secret: "3Fmr0ZoNQ6gsrCm62fkH4JjpT5wZxxc6NXoVtm1RJfiMB"
-)
